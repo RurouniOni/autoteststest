f0fc9df6ade2a019b5bc00eae9fc895bfbc7c9e3
RurouniOni <vladyslav.shyb@gmail.com>
2026-01-18 22:54:30 +0000

    new commit

--- test1.py
+++ test1.py
@@ -1,12 +1,26 @@
 from selenium import webdriver
-
-driver_location = "/snap/bin/chromium"
-binary_location = "/usr/bin/chromium-browser"
+from selenium.webdriver.common.by import By
+from time import sleep
+from selenium.webdriver.common.keys import Keys
 
 options = webdriver.ChromeOptions()
-options.binary_location = binary_location
 driver = webdriver.Chrome(options=options)
 
-driver.get("https://www.google.com")
-print(driver.page_sourceencode('utf8'))
-driver.quit()
+try:
+    driver.get("https://www.google.com")
+    sleep(2)
+    search_box = driver.find_element(By.NAME, "q") #def name for google searching
+    search_box.send_keys("Python Selenium") # print text
+    sleep(2)
+    search_box.send_keys(Keys.ENTER)
+    sleep(2)
+    #site_1 = driver.find_element(By.XPATH, value="//*[contains(text(),'Python Selenium')]")
+    sleep(2)
+    print(f"URL: {driver.current_url}")
+    print(f"Page_title: {driver.title}")
+
+except Exception as e:
+    print(f"Error: {e}")
+
+finally:
+    driver.quit()
